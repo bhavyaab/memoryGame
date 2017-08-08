809144e25340429b6ef597f49ba79fbd1bc2a8f5
Bhavya Bharti <bhavyab@outlook.com>
2017-08-07 17:02:57 -0700

    working react webpack

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const dotenv = require('dotenv').load();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(`${__dirname}/build`));
+
+app.listen(PORT, function(){
+  console.log('server up:', PORT);
+});
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require('path');
+var htmlPlugin = require('html-webpack-plugin');
+
+var DIST_DIR = path.resolve(__dirname, 'build');
+var SRC_DIR = path.resolve(__dirname, 'src');
+
+let plugins = [
+  new htmlPlugin({template : `${__dirname}/src/index.html`})
+];
+
+var config = {
+  plugins,
+  entry: SRC_DIR + '/app/index.js',
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.js$/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-2']
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
