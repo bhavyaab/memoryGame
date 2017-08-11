b4419e8e6acbf4a8ee81186ef068e74c7f7eb6db
Bhavya Bharti <bhavyab@outlook.com>
2017-08-11 12:56:28 -0700

    trigger event

--- src/app/component/Home.js
+++ src/app/component/Home.js
@@ -1,30 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 export class Home extends React.Component {
+  constructor(props){
+    super();
+    this.score = props.user.score;
+  }
+  addScore (){
+    console.log(' Add score called and new score is ', this.score);
+    this.score += 3;
+  }
   render(){
-    console.log('props - ', this.props)
-    var text = 'some text'
+    console.log('props - ', this.props);
     return (
       <div>
            <p>new Home Component!</p>
-           <p>Your name: {this.props.name}</p>
-           <p>Your age: {this.props.age} </p>
-           <p> {text} </p>
             <p>
              <span>User Check: user name is - {this.props.user.name} and age is: {this.props.user.age}</span>
+             <li>Score: {this.score}</li>
             </p>
              <h5>User hobbies are:</h5>
                <ul>
                  {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
               </ul>
-                 {this.props.children}
+            {/* One way off spassing event */}
+           <button className="btn btn-primary" onClick={ (e) => this.addScore(e)}>Add Score </button>
+            <p>new score : {this.score}</p>
+            {/*other way of passing event*/}
+            <button className="btn btn-primary" onClick={this.addScore.bind(this)}>Add Score </button>
+             <p>new score : {this.score} : {this.props.user.score}</p>
       </div>
-    )
+    );
   }
 }
 
 Home.propTypes = {
-  name: React.PropTypes.string,
-  age: React.PropTypes.number,
   user: React.PropTypes.object,
-}
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,18 @@
 
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-import {Header} from './component/Header'
-import { Home } from './component/Home'
+import {Header} from './component/Header';
+import { Home } from './component/Home';
 
 class App extends React.Component {
   render (){
     var user ={
       name: 'Ana',
       age: 34,
+      score: 0,
       hobbies: ['Sports', 'Banana', 'Grapes'],
-    }
+    };
     return (
       <div className="container">
         <div className="row">
@@ -21,11 +22,11 @@ class App extends React.Component {
          </div>
       <div className="row">
          <div className="col-xs-10 col-xs-offset-1">
-            <Home name={'Max'} age={27} user={user}/>
+            <Home user={user}/>
           </div>
        </div>
       </div>
-    )
+    );
   }
 }
-render(<App/>, document.getElementById('root'))
+render(<App/>, document.getElementById('root'));
