17af3d2600418e3abf856a71f601b7b8f85f5fea
Bhavya Bharti <bhavyab@outlook.com>
2017-11-09 13:32:48 -0800

    position centered

--- src/component/blockImage/index.js
+++ src/component/blockImage/index.js
@@ -21,17 +21,27 @@ export class BlockImage extends React.Component{
       for(var j = 0; j < 4; j ++){
         if(!(((i == 1) && (j == 1)) || ((i == 1) && (j == 2)) || ((i == 2) && (j == 1)) || ((i == 2) && (j == 2)))){
           element = <OneImage
-            style={{top: `${i * 22.5}%`,right: `${j * 22.5}%`, position: `absolute`}}
-            image={this.state.images[combinationArray[count]]}/>
+            style={{
+              top: `${i * 22.5}%`,
+              right: `${j * 22.5}%`,
+              position: `absolute`}}
+            image={this.state.images[combinationArray[count]]} />
           allImage[count] = element
           count++
         }
       }
     }
-    var allImage = generateCombination()
     return (
       <div className="blockImage">
         {allImage}
+        <OneImage
+          style={{
+            top: `22.5%`,
+            right: `22.5%`,
+            height: '42.5%',
+            width: '42.5%',
+            position: `absolute`}}
+          image={'../../data/apple.jpg'} />
       </div>
     )
   }
