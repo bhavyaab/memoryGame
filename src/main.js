7e0fec9ec67892badf77f2abec3105af8b3ff565
Bhavya Bharti <bhavyab@outlook.com>
2017-12-07 17:25:14 -0800

    loading icon

--- src/main.js
+++ src/main.js
@@ -4,31 +4,20 @@ import ReactDom from 'react-dom'
 import App from './component/app'
 
 ReactDom.render(<App />, document.getElementById('root'))
-// myFunction()
-//
-// function myFunction() {
-//   setTimeout(showPage, 3)
-// }
-//
-// function showPage() {
-//   document.getElementsByClassName('loader')[0].style.display = 'none'
-//   document.getElementById('root').style.display = 'block'
-// }
-
 
 var loading = {
   start: function() {
     document.body.insertAdjacentHTML('beforeend', '<div id="loader"></div>')
+    document.getElementById('root').style.display = 'none'
   },
   complete: function() {
     var loading = document.getElementById('loader')
-    console.log('loading')
     loading.remove(loading)
+    document.getElementById('root').style.display = 'block'
   },
 }
 loading.start()
 document.addEventListener('readystatechange', function() {
-  console.log(document.readyState)
   if (document.readyState === 'complete') {
     loading.complete()
   }
