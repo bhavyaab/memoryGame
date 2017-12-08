7105193cd004e031483e737207a1e666bdbfc7cc
Bhavya Bharti <bhavyab@outlook.com>
2017-12-07 17:05:01 -0800

    loader adde to test

--- src/main.js
+++ src/main.js
@@ -4,3 +4,32 @@ import ReactDom from 'react-dom'
 import App from './component/app'
 
 ReactDom.render(<App />, document.getElementById('root'))
+// myFunction()
+//
+// function myFunction() {
+//   setTimeout(showPage, 3)
+// }
+//
+// function showPage() {
+//   document.getElementsByClassName('loader')[0].style.display = 'none'
+//   document.getElementById('root').style.display = 'block'
+// }
+
+
+var loading = {
+  start: function() {
+    document.body.insertAdjacentHTML('beforeend', '<div id="loading">LOADING</div>')
+  },
+  complete: function() {
+    var loading = document.getElementById('loading')
+    console.log(loading)
+    loading.remove(loading)
+  },
+}
+loading.start()
+document.addEventListener('readystatechange', function() {
+  console.log(document.readyState)
+  if (document.readyState === 'complete') {
+    loading.complete()
+  }
+})
